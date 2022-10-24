18f70c95ca0338a85791738dfbddc69b87fff8ae
Andrea Storace <a.storace@bitmama.it>
2022-10-24 11:49:04 +0200

    Updates

---
+++ embedded.js
@@ -0,0 +1,166 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/pages/embedded.js":
+/*!******************************!*\
+  !*** ./js/pages/embedded.js ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_pages_flow_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../../styles/pages/flow.css */ \"./styles/pages/flow.css\");\n/* harmony import */ var _styles_pages_sectioning_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../styles/pages/sectioning.css */ \"./styles/pages/sectioning.css\");\n/* harmony import */ var _styles_pages_phrasing_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../styles/pages/phrasing.css */ \"./styles/pages/phrasing.css\");\n/* harmony import */ var _styles_pages_embedded_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../styles/pages/embedded.css */ \"./styles/pages/embedded.css\");\n/* harmony import */ var _media_Bitmama_Reply_Wallpaper_1080p_b_jpg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../../media/Bitmama-Reply-Wallpaper_1080p_b.jpg */ \"./media/Bitmama-Reply-Wallpaper_1080p_b.jpg\");\n/* harmony import */ var _media_Bitmama_Reply_Wallpaper_1200p_b_jpg__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../../media/Bitmama-Reply-Wallpaper_1200p_b.jpg */ \"./media/Bitmama-Reply-Wallpaper_1200p_b.jpg\");\n/* harmony import */ var _media_Bitmama_Reply_Wallpaper_2100p_b_jpg__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../../media/Bitmama-Reply-Wallpaper_2100p_b.jpg */ \"./media/Bitmama-Reply-Wallpaper_2100p_b.jpg\");\n/* harmony import */ var _media_kisspng_susuwatari_studio_ghibli_ghibli_museum_clip_art_neighbour_5b33be013b4819_5791045515301176332428_png__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../../media/kisspng-susuwatari-studio-ghibli-ghibli-museum-clip-art-neighbour-5b33be013b4819.5791045515301176332428.png */ \"./media/kisspng-susuwatari-studio-ghibli-ghibli-museum-clip-art-neighbour-5b33be013b4819.5791045515301176332428.png\");\n\n\n\n\n\n\n\n\n\n//# sourceURL=webpack:///./js/pages/embedded.js?");
+
+/***/ }),
+
+/***/ "./media/Bitmama-Reply-Wallpaper_1080p_b.jpg":
+/*!***************************************************!*\
+  !*** ./media/Bitmama-Reply-Wallpaper_1080p_b.jpg ***!
+  \***************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"media/Bitmama-Reply-Wallpaper_1080p_b.jpg\");\n\n//# sourceURL=webpack:///./media/Bitmama-Reply-Wallpaper_1080p_b.jpg?");
+
+/***/ }),
+
+/***/ "./media/Bitmama-Reply-Wallpaper_1200p_b.jpg":
+/*!***************************************************!*\
+  !*** ./media/Bitmama-Reply-Wallpaper_1200p_b.jpg ***!
+  \***************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"media/Bitmama-Reply-Wallpaper_1200p_b.jpg\");\n\n//# sourceURL=webpack:///./media/Bitmama-Reply-Wallpaper_1200p_b.jpg?");
+
+/***/ }),
+
+/***/ "./media/Bitmama-Reply-Wallpaper_2100p_b.jpg":
+/*!***************************************************!*\
+  !*** ./media/Bitmama-Reply-Wallpaper_2100p_b.jpg ***!
+  \***************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"media/Bitmama-Reply-Wallpaper_2100p_b.jpg\");\n\n//# sourceURL=webpack:///./media/Bitmama-Reply-Wallpaper_2100p_b.jpg?");
+
+/***/ }),
+
+/***/ "./media/kisspng-susuwatari-studio-ghibli-ghibli-museum-clip-art-neighbour-5b33be013b4819.5791045515301176332428.png":
+/*!***************************************************************************************************************************!*\
+  !*** ./media/kisspng-susuwatari-studio-ghibli-ghibli-museum-clip-art-neighbour-5b33be013b4819.5791045515301176332428.png ***!
+  \***************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"media/kisspng-susuwatari-studio-ghibli-ghibli-museum-clip-art-neighbour-5b33be013b4819.5791045515301176332428.png\");\n\n//# sourceURL=webpack:///./media/kisspng-susuwatari-studio-ghibli-ghibli-museum-clip-art-neighbour-5b33be013b4819.5791045515301176332428.png?");
+
+/***/ }),
+
+/***/ "./styles/pages/embedded.css":
+/*!***********************************!*\
+  !*** ./styles/pages/embedded.css ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./styles/pages/embedded.css?");
+
+/***/ }),
+
+/***/ "./styles/pages/flow.css":
+/*!*******************************!*\
+  !*** ./styles/pages/flow.css ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./styles/pages/flow.css?");
+
+/***/ }),
+
+/***/ "./styles/pages/phrasing.css":
+/*!***********************************!*\
+  !*** ./styles/pages/phrasing.css ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./styles/pages/phrasing.css?");
+
+/***/ }),
+
+/***/ "./styles/pages/sectioning.css":
+/*!*************************************!*\
+  !*** ./styles/pages/sectioning.css ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./styles/pages/sectioning.css?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./js/pages/embedded.js");
+/******/ 	
+/******/ })()
+;
